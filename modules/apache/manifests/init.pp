643403704bc3705269f54ab3bce739820c613df4
Joseph Clark <ClarkCode@gmail.com>
2014-11-15 18:52:14 -0500

    Updated Manifiest to use VHost

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -13,38 +13,24 @@ class apache {
     require => Package["apache2"]
   }
 
-  # create directory
-  file {"/etc/apache2/sites-enabled":
-    ensure => directory,
-    recurse => true,
-    purge => true,
-    force => true,
-    before => File["/etc/apache2/sites-enabled/vagrant_webroot"],
-    require => Package["apache2"],
-  }
 
   # create apache config from main vagrant manifests
-  file { "/etc/apache2/sites-available/vagrant_webroot":
-    ensure => present,
-    source => "/vagrant/manifests/vagrant_webroot",
+  file { 'site-config':
+    path    => '/etc/apache2/sites-enabled/000-default',
+    source  => '/vagrant/site-config',
     require => Package["apache2"],
   }
 
-  # symlink apache site to the site-enabled directory
-  file { "/etc/apache2/sites-enabled/vagrant_webroot":
-    ensure => link,
-    target => "/etc/apache2/sites-available/vagrant_webroot",
-    require => File["/etc/apache2/sites-available/vagrant_webroot"],
-    notify => Service["apache2"],
+  # starts the apache2 service
+  service { 'apache2':
+    ensure      => 'running',
+    hasrestart  => true,
+    subscribe   => File["site-config"]
   }
 
-  # starts the apache2 service once the packages installed, and monitors changes to its configuration files and reloads if nesessary
-  service { "apache2":
-    ensure => running,
-    require => Package["apache2"],
-    subscribe => [
-      File["/etc/apache2/mods-enabled/rewrite.load"],
-      File["/etc/apache2/sites-available/vagrant_webroot"]
-    ],
+  # Create the Apache landing Page
+  file { '/vagrant/index.html':
+    content => "<h1>Vagrant + Puppet</h1>",
   }
 }
+
