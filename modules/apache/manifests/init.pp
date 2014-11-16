bb425407a90d8d72b4267158dd18fa199681dba2
Joseph Clark <ClarkCode@gmail.com>
2014-11-16 11:13:35 -0500

    Renamed to vagrant_webroot to vagrant_webroot.conf

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,36 +1,50 @@
 class apache {
 
-  # install apache
+# install apache
   package { "apache2":
     ensure => present,
     require => Exec["apt-get update"]
   }
 
-  # ensures that mode_rewrite is loaded and modifies the default configuration file
+# ensures that mode_rewrite is loaded and modifies the default configuration file
   file { "/etc/apache2/mods-enabled/rewrite.load":
     ensure => link,
     target => "/etc/apache2/mods-available/rewrite.load",
     require => Package["apache2"]
   }
 
-
-  # create apache config from main vagrant manifests
-  file { 'site-config':
-    path    => '/etc/apache2/sites-enabled/000-default',
-    source  => '/vagrant/site-config',
+# create directory
+  file {"/etc/apache2/sites-enabled":
+    ensure => directory,
+    recurse => true,
+    purge => true,
+    force => true,
+    before => File["/etc/apache2/sites-enabled/vagrant_webroot.conf"],
     require => Package["apache2"],
   }
 
-  # starts the apache2 service
-  service { 'apache2':
-    ensure      => 'running',
-    hasrestart  => true,
-    subscribe   => File["site-config"]
+# create apache config from main vagrant manifests
+  file { "/etc/apache2/sites-available/vagrant_webroot.conf":
+    ensure => present,
+    source => "/vagrant/manifests/vagrant_webroot.conf",
+    require => Package["apache2"],
   }
 
-  # Create the Apache landing Page
-  file { '/vagrant/index.html':
-    content => "<h1>Vagrant + Puppet</h1>",
+# symlink apache site to the site-enabled directory
+  file { "/etc/apache2/sites-enabled/vagrant_webroot.conf":
+    ensure => link,
+    target => "/etc/apache2/sites-available/vagrant_webroot.conf",
+    require => File["/etc/apache2/sites-available/vagrant_webroot.conf"],
+    notify => Service["apache2"],
+  }
+
+# starts the apache2 service once the packages installed, and monitors changes to its configuration files and reloads if nesessary
+  service { "apache2":
+    ensure => running,
+    require => Package["apache2"],
+    subscribe => [
+      File["/etc/apache2/mods-enabled/rewrite.load"],
+      File["/etc/apache2/sites-available/vagrant_webroot.conf"]
+    ],
   }
 }
-
